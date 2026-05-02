8d0c7f42b9d1c87934cbffc043427541ed4fe560
horder6947 <kelrefaey00@gmail.com>
2026-05-02 09:44:18 +0300

    done statistic 2

--- project-phase-2/repos/statistics.js
+++ project-phase-2/repos/statistics.js
@@ -3,26 +3,26 @@ import { getFollowersCount } from "@/repos/follow";
 
 
 export async function getAverageFollowersPerUser() {
-    
+
   try {
-   
+
     const userList = await prisma.user.findMany({ select: { id: true } });
 
-    
+
     if (!userList || userList.length === 0) {
       return { averageFollowersPerUser: 0, totalFollowers: 0, totalUsers: 0 };
     }
 
-    
+
     const followerCounts = await Promise.all(
       userList.map(async (user) => {
         const result = await getFollowersCount(user.id);
-       
+
         return typeof result === "number" ? result : result?._count || 0;
       })
     );
 
-    
+
     const totalFollowers = followerCounts.reduce((sum, n) => sum + n, 0);
     const totalUsers = userList.length;
     const averageFollowersPerUser = totalUsers > 0 ? (totalFollowers / totalUsers).toFixed(2) : 0;
@@ -31,9 +31,20 @@ export async function getAverageFollowersPerUser() {
 
 
   } catch (err) {
-  
+
     console.error("Error in getAverageFollowersPerUser:", err);
     throw err;
   }
 }
 
+export async function getAveragePostsPerUser() {
+  const postsCount = await prisma.post.aggregate({
+    _count: true,
+  });
+  const usersCount = await prisma.user.aggregate({
+    _count: true
+  });
+  if (usersCount !== 0)
+    return postsCount / usersCount;
+  return 0;
+}
