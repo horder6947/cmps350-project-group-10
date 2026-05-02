edcebc1b7081a6529c6286f45a9e137d28092b3b
Yacine Merabet <yacimerabet@gmail.com>
2026-05-02 16:48:22 +0300

    fix: generate Prisma client on build and use Turso env vars

--- project-phase-2/prisma/seed.mjs
+++ project-phase-2/prisma/seed.mjs
@@ -4,7 +4,8 @@ import { faker } from "@faker-js/faker";
 
 const prisma = new PrismaClient({
     adapter: new PrismaLibSql({
-        url: process.env.DATABASE_URL ?? "",
+        url: process.env.TURSO_DATABASE_URL ?? "",
+        authToken: process.env.DATABASE_AUTH_TOKEN,
     }),
     log: ["query"],
 });
--- project-phase-2/repos/prisma.js
+++ project-phase-2/repos/prisma.js
@@ -3,7 +3,8 @@ import { PrismaClient } from "@/prisma/client/client";
 
 export default new PrismaClient({
     adapter: new PrismaLibSql({
-        url: process.env.DATABASE_URL ?? "",
+        url: process.env.TURSO_DATABASE_URL ?? "",
+        authToken: process.env.DATABASE_AUTH_TOKEN,
     }),
     log: ["query"],
 });
