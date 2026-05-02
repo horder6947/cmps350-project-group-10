13b9ecc854c45252eb1105a4370de4d45dea7d15
horder6947 <kelrefaey00@gmail.com>
2026-05-02 13:32:21 +0300

    edit seed.mjs to assign random dates, modify folder structure

--- project-phase-2/repos/test.js
+++ project-phase-2/repos/test.js
@@ -6,4 +6,4 @@ import * as comment from "@/repos/comment";
 import * as like from "@/repos/like";
 import * as stats from "@/repos/statistics";
 
-console.log(await stats.getMostLikedPosts(5));
+console.log(await stats.getNewUsers());
